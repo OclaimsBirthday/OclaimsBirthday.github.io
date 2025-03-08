b1591c2ef4cf17bbbaa7cd04bb095b27c079e9c6
OclaimsBirthday <oclaimsbirthday@gmail.com>
2025-03-08 19:42:49 +0100

    Added function and comments
    Added some more TODOS and inserted the fetch promisechain into a function, so it doesnt just auto run without a purpose. Not sure what to do with it yet since I dont have a backend yet but... :3

--- JS/Index.js
+++ JS/Index.js
@@ -1,4 +1,9 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function TestFetch() {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+}
+//Test procedure testing fetch requests. Might replace with WebSockets so I can have it be Live Updated when I get my Raspberry Pi 5 :3
+
